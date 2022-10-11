22e995e0d8ea07fa59ba613d9925b2cf9e2ef935
wonder-H <soimar@naver.com>
2022-10-11 22:06:01 +0900

    feat: API GET메소드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
 import './App.css';
+import { useEffect } from 'react';
+import { insApi } from './utils/api';
 
 function App() {
+  const newsListsId = [];
+  const newsLists = [];
+
+  async function getNewsLists() {
+    try {
+      const res = await insApi.get('/topstories.json');
+      if (res.statusText !== 'OK') {
+        throw new Error('Could not fetch data :-/ ');
+      }
+      return res.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getNewsItems() {
+    for (let i = 0; i < 10; i++) {
+      await insApi.get(`/item/${newsListsId[i]}.json`).then((res) => newsLists.push(res.data));
+    }
+    console.log(newsLists);
+  }
+
+  useEffect(() => {
+    getNewsLists().then((res) => {
+      res.forEach((id: number) => {
+        newsListsId.push(id);
+      });
+      getNewsItems();
+    });
+  }, []);
+
   return <div className="App"></div>;
 }
 

a8147e427cd2b22789c5aa9e79d9e39f8e1d3470
wonder-H <soimar@naver.com>
2022-10-12 16:31:17 +0900

    feat: Header컴포넌트 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">Best | </a>
+        <a href="#">News | </a>
+        <a href="#">Ask | </a>
+        <a href="#">Jobs</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

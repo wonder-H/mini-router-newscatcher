e92b90ef65fca47bb24728eda2e547b4d765aef4
wonder-H <soimar@naver.com>
2022-10-13 17:32:08 +0900

    feat: 라우트 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState, useContext } from 'react';
 import { insApi } from './utils/api';
 import NewsLists from './components/NewsLists';
+import MainPage from './pages/MainPage';
+import AskPage from './pages/AskPage';
+import JobsPage from './pages/JobsPage';
+import BestPage from './pages/BestPage';
+import NewsPage from './pages/NewsPage';
+
 import Header from './components/Header';
 function App() {
   const [newsListsId, setNewsListsId] = useState(null);
@@ -47,17 +53,73 @@ function App() {
     }
   }, [newsListsId]);
 
+  const RouterContext = createContext({
+    routePath: '',
+    changePath: () => null,
+  });
+
+  const routeHandler = (e, to) => {
+    const { changePath } = useContext(routerContext);
+    e.preventDefault();
+    changePath(to);
+  };
+
+  const Router = ({ to, children }) => {
+    const [routePath, setRoutePath] = useState(location.pathname);
+    const contextValue = {
+      routePath,
+      changePath: setRoutePath,
+    };
+
+    if (routePath === '/best') {
+      return (
+        <RouterContext.Provider value={contextValue}>
+          <BestPage to={to} onClick={(e) => routeHandler(e, to)}>
+            {children}
+          </BestPage>
+        </RouterContext.Provider>
+      );
+    } else if (routePath === '/news') {
+      return (
+        <NewsPage to={to} onClick={(e) => routeHandler(e, to)}>
+          {children}
+        </NewsPage>
+      );
+    } else if (routePath === '/ask') {
+      return (
+        <AskPage to={to} onClick={(e) => routeHandler(e, to)}>
+          {children}
+        </AskPage>
+      );
+    } else if (routePath === '/jobs') {
+      return (
+        <JobsPage to={to} onClick={(e) => routeHandler(e, to)}>
+          {children}
+        </JobsPage>
+      );
+    } else {
+      return (
+        <MainPage to={to} onClick={(e) => routeHandler(e, to)}>
+          {children}
+        </MainPage>
+      );
+    }
+  };
+  const { routePath } = useContext(RouterContext);
+
   return (
     <div className="App">
       <Header />
+      <Router>{routePath === '/news' && <NewsPage />}</Router>
       {/* <NewsLists /> */}
-      {newsLists.map((list) => (
+
+      {/* {newsLists.map((list) => (
         <div key={list.id}>
           <p>id: {list.id}</p>
           <p>title: {list.title}</p>
           <p>time: {list.time}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }

d1bf18781ffc9d1cc0c56a4ff0ea71effabbe36a
wonder-H <soimar@naver.com>
2022-10-14 22:03:48 +0900

    Design: 기본 CSS 적용

--- src/components/NewsListItem.tsx
+++ src/components/NewsListItem.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+  width: 100%;
+  max-width: 100%;
+  height: 100px;
+  padding: 2em;
+  box-sizing: border-box;
+  border: 2px solid rgba(190, 189, 255, 0.6);
+  border-radius: 10px;
+  outline: none;
+  font-size: 13px;
+  color: #444;
+  background: #ffffff;
+  resize: vertical;
+  box-shadow: 0px 5px 10px 0px rgba(115, 158, 179, 0.2);
+`;
 function NewsListItem() {
-  return <div>NewsListItem</div>;
+  return <StyledDiv>NewsListItem</StyledDiv>;
 }
 
 export default NewsListItem;
--- src/pages/AskPage.tsx
+++ src/pages/AskPage.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { listsIdSelector } from '../state';
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  box-sizing: border-box;
+  margin: 50px auto;
+  width: 80%;
+  height: 90vh;
+
+  div {
+    width: 100%;
+    max-width: 100%;
+    height: 200px;
+    padding: 2em;
+    box-sizing: border-box;
+    border: 2px solid rgba(233, 196, 178, 0.6);
+    border-radius: 10px;
+    outline: none;
+    font-size: 13px;
+    color: #444;
+    background: #ffffff;
+    resize: vertical;
+    box-shadow: 0px 5px 20px 0px rgba(126, 99, 99, 0.2);
+  }
+`;
 
 function AskPage() {
   const lists = useRecoilValue(listsIdSelector);
 
   return (
-    <div>
-      <h2>AskPage</h2>
-      {lists}
-    </div>
+    <StyledDiv>
+      {/* {lists} */}
+      <div>
+        <h2>AskPage</h2>
+      </div>
+    </StyledDiv>
   );
 }
 
